69788e5da247a44f4fece4c9761b123769f4ef1b
rakawidjaja169 <raka.widjaja169@gmail.com>
2022-05-29 21:10:42 +0700

    First Commit

---
+++ hello-world/src/components/LogosVue.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Find the Best Logo in Town</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'LogosVue'
+}
+</script>
+
+<style scoped>
+
+</style>
