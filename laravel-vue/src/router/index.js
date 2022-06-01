2a1e4711d7a67477bc380b59b0f7d398d0b9ebc7
rakawidjaja169 <raka.widjaja169@gmail.com>
2022-06-02 00:23:27 +0700

    Update V-Bind Product

--- laravel-vue/src/router/index.js
+++ laravel-vue/src/router/index.js
@@ -11,6 +11,14 @@ const routes = [
     component: HomeView
   },
   {
+    path: '/product',
+    name: 'product',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/ProductView.vue')
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
