764e991b1884946a76ae0431235a518723262cf8
rakawidjaja169 <raka.widjaja169@gmail.com>
2022-06-01 22:15:58 +0700

    Update Home Laravel Vue
    - Home

---
+++ laravel-vue/src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+
+    <HeaderView />
+
+    <CarouselView />
+
+    <ProductSlider />
+
+    <InstagramGallery />
+
+    <PartnerLogo />
+
+    <FooterView />
+
+  </div>
+</template>
+
+<script>
+
+import HeaderView from "@/components/HeaderView.vue";
+import CarouselView from "@/components/CarouselView.vue";
+import ProductSlider from "@/components/ProductSlider.vue";
+import InstagramGallery from "@/components/InstagramGallery.vue";
+import PartnerLogo from "@/components/PartnerLogo.vue";
+import FooterView from "@/components/FooterView.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HeaderView,
+    CarouselView,
+    ProductSlider,
+    InstagramGallery,
+    PartnerLogo,
+    FooterView
+  },
+};
+</script>
